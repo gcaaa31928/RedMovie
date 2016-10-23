55fac6358fc203dd1025e1decc6759370aabf73f
gcaaa31928 <gcaaa31928@gmail.com>
2016-10-24 03:25:51 +0800

    added blank of movie detail activity

---
+++ app/src/main/java/com/red/redmovie/movie/widget/MovieDetailActivity.java
@@ -0,0 +1,15 @@
+package com.red.redmovie.movie.widget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.red.redmovie.R;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
